0dac42ee6fe9925c0154f6d6edc357af8d7da33a
gabwink <gabriellewink2025@u.northwestern.edu>
2023-07-18 14:13:31 -0500

    fixed spawn and destination cell creation

--- src/tool/create_predator_destinations.cpp
+++ src/tool/create_predator_destinations.cpp
@@ -11,22 +11,23 @@ using namespace json_cpp;
 
 
 int main (int argc, char **argv){
-    string new_world = "22_01";    // input new world naem here or as program argument
+    string new_world = "21_05";    // input new world name here or as program argument
 
     Parser p(argc,argv);
-    auto occlusions = p.get(Key("-o","--occlusions"), new_world);
+    auto occlusions = p.get(Key("-o","--occlusions"),new_world);
     World world = World::get_from_parameters_name("hexagonal","canonical", occlusions);
     Graph graph = world.create_graph();
     Cell_group cells = world.create_cell_group().free_cells();
+    Graph g = world.create_graph();
     Cell_group pd;
     for (const Cell &cell:cells) {
-        if (graph.is_connected(cell,world.cells[0])){
+        if (graph.is_connected(cell,world.cells[0]) && g[cell].size() == world.connection_pattern.size()){
             pd.add(cell);
         }
     }
 
 
-    pd.save("../../cellworld_data/cell_group/hexagonal."+ occlusions +".predator_destinations");  // MAKE SURE WORLD ARE IS NAMED PROPERLY
+//    pd.save("../../cellworld_data/cell_group/hexagonal."+ occlusions +".predator_destinations");  // MAKE SURE WORLD ARE IS NAMED PROPERLY
     cout << "done....." << endl;
     cout << " New predator destinations created for: " << occlusions  << endl;
     cout << pd << endl;
--- src/tool/create_spawn_locations.cpp
+++ src/tool/create_spawn_locations.cpp
@@ -8,15 +8,16 @@ using namespace json_cpp;
 
 
 int main (int argc, char **argv){
-    string new_world = "22_01";
+    string new_world = "21_05";
 
     Parser p(argc,argv);
-    auto occlusions = p.get(Key("-o","--occlusions"), new_world);
+    auto occlusions = p.get(Key("-o","--occlusions"),new_world);
     auto threshold = stof(p.get(Key("-d","--distance"),"0.5"));
     World world = World::get_from_parameters_name("hexagonal","canonical", occlusions);
 
     auto cells = world.create_cell_group();
     Cell_group pd = world.create_cell_group(Resources::from("cell_group").key("hexagonal").key(occlusions).key("predator_destinations").get_resource<Cell_group_builder>());
+    Graph g = world.create_graph();
     Graph visibility = world.create_graph(Resources::from("graph").key("hexagonal").key(occlusions).key("cell_visibility").get_resource<Graph_builder>());
     Cell_group spawn_locations;
     Map map(cells);
@@ -24,13 +25,13 @@ int main (int argc, char **argv){
     auto &start_location_visibility = visibility[start_location];
     for (const Cell &cell:pd) {
         if (cell.location.dist(start_location.location) > threshold &&
-            !start_location_visibility.contains(cell)) {
+            !start_location_visibility.contains(cell) && g[cell].size() == world.connection_pattern.size()) {
             spawn_locations.add(cell);
         }
     }
 
 
-    spawn_locations.save("../../cellworld_data/cell_group/hexagonal."+ occlusions +".spawn_locations");  // MAKE SURE WORLD ARE IS NAMED PROPERLY
+//    spawn_locations.save("../../cellworld_data/cell_group/hexagonal."+ occlusions +".spawn_locations");  // MAKE SURE WORLD ARE IS NAMED PROPERLY
     cout << "done....." << endl;
     cout << " New spawn locations created for: " << occlusions  << endl;
     cout << spawn_locations << endl;
