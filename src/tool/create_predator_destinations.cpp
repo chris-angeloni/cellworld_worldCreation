f48c66ed843285d7b840804d2de0d35ce5a5df38
Chris Angeloni <3865975+chris-angeloni@users.noreply.github.com>
2024-10-25 16:51:30 -0500

    Update create_predator_destinations.cpp

--- src/tool/create_predator_destinations.cpp
+++ src/tool/create_predator_destinations.cpp
@@ -2,33 +2,28 @@
 #include <params_cpp.h>
 #include <iostream>
 
-
-
 using namespace params_cpp;
 using namespace cell_world;
 using namespace std;
 using namespace json_cpp;
 
-
 int main (int argc, char **argv){
-    string new_world = "21_05";    // input new world name here or as program argument
-
     Parser p(argc,argv);
-    auto occlusions = p.get(Key("-o","--occlusions"),new_world);
-    World world = World::get_from_parameters_name("hexagonal","canonical", occlusions);
+    auto occlusions = p.get(Key("-o","--occlusions"),"");
+    auto folder = Resources::cache_folder();
+    auto configuration = p.get(Key("-c","--configuration"),"hexagonal");
+    auto output_file = p.get(Key("-of","--output_file"),folder + "/cell_group/" + configuration + "." + occlusions + ".predator_destinations");
+
+    World world = World::get_from_parameters_name(configuration,"canonical", occlusions);
     Graph graph = world.create_graph();
     Cell_group cells = world.create_cell_group().free_cells();
     Graph g = world.create_graph();
     Cell_group pd;
     for (const Cell &cell:cells) {
-        if (graph.is_connected(cell,world.cells[0]) && g[cell].size() == world.connection_pattern.size()){  // if the cell is connected to the entrance and surrounded by free cells add it to destination list
+        if (graph.is_connected(cell,world.cells[0]) && g[cell].size() == world.connection_pattern.size()){
             pd.add(cell);
         }
     }
-
-
-    pd.save("./hexagonal."+ occlusions +".predator_destinations");  // MAKE SURE WORLD ARE IS NAMED PROPERLY
-    cout << "done....." << endl;
-    cout << " New predator destinations created for: " << occlusions  << endl;
-    cout << pd << endl;
+    pd.save(output_file);
 }
+
