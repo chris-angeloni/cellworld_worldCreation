2d640fdec41832af2a6ad51caabaed51524982b7
Chris Angeloni <3865975+chris-angeloni@users.noreply.github.com>
2024-10-25 17:02:31 -0500

    Update create_visibility.cpp

--- src/tool/create_visibility.cpp
+++ src/tool/create_visibility.cpp
@@ -7,20 +7,15 @@ using namespace std;
 using namespace json_cpp;
 
 int main (int argc, char **argv){
-    string new_world = "oasis_14_02_padded";
-
     Parser p(argc,argv);
-    auto occlusions = p.get(Key("-o","--occlusions"),new_world);
+    auto occlusions = p.get(Key("-o","--occlusions"),"");
     auto configuration = p.get(Key("-c","--configuration"),"hexagonal");
+    auto folder = Resources::cache_folder();
+    auto output_file = p.get(Key("-of","--output_file"),folder + "/graph/" + configuration + "." + occlusions + ".cell_visibility");
     World world = World::get_from_parameters_name(configuration,"canonical", occlusions);
     Cell_group cells = world.create_cell_group();
     auto cell_shape = world.get_configuration().cell_shape;
     auto cell_transformation = world.get_implementation().cell_transformation;
     auto visibility = Coordinates_visibility::create_graph(cells,cell_shape,cell_transformation);
-
-    visibility.save("./hexagonal."+ occlusions +".cell_visibility");  // MAKE SURE WORLD ARE IS NAMED PROPERLY
-    cout << "done....." << endl;
-    cout << "Create visibility for: " << occlusions  << endl;
-
-    cout << visibility << endl;
+    visibility.save(output_file);
 }
